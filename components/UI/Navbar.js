529fcc5978913a62f94c990dcec293f70fe1c83d
ataliesavista <ataliesavista02@gmail.com>
2025-01-23 09:20:56 +0700

    fix(navbar): membenarkan tampilan navbar

--- components/UI/Navbar.js
+++ components/UI/Navbar.js
@@ -5,7 +5,7 @@ import Link from 'next/link';
 import nesco from '@/public/nesco.png';
 import Image from 'next/image';
 import { IoIosArrowDown } from 'react-icons/io';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const routes = [
   {
@@ -42,11 +42,11 @@ const routes = [
 
 /* Navbar Desktop */
 const DesktopMenu = ({ openDropdown, toggleMainDropdown }) => (
-  <ul className="items-center space-x-[4vw] text-[1.7vw] font-bold text-darkblue md:flex 2xl:text-[1.1vw]">
+  <ul className="items-center space-x-[4vw] text-[1.8vw] font-bold text-darkblue md:flex xl:text-[1.2vw]">
     {routes.map((route, index) => (
       <li
         key={index}
-        className="group relative leading-[4vw]"
+        className="group relative leading-[7.3vw] xl:leading-[5.3vw]"
         onBlur={() => toggleMainDropdown(null)}
       >
         {route.child ? (
@@ -80,7 +80,7 @@ const DesktopMenu = ({ openDropdown, toggleMainDropdown }) => (
                 {route.child.map((child, childIndex) => (
                   <li
                     key={childIndex}
-                    className="text-nowrap py-[0.5vw] !font-montserrat text-[1.7vw] !leading-none opacity-70 duration-200 ease-in-out hover:opacity-100 2xl:text-[1.1vw]"
+                    className="text-nowrap py-[0.5vw] !font-montserrat text-[1.6vw] !leading-none opacity-70 duration-500 ease-in-out hover:opacity-100 xl:text-[1.1vw]"
                   >
                     <Link href={child.href} className="block">
                       {child.name}
@@ -107,10 +107,10 @@ const DesktopMenu = ({ openDropdown, toggleMainDropdown }) => (
 const MobileMenu = ({ openDropdown, toggleMainDropdown, openChild, toggleChildDropdown }) => (
   <div className="md:hidden">
     <div className="relative mx-auto flex items-center justify-between px-6 py-2">
-      <div className="flex flex-row items-center gap-[1vw] text-[5vw] font-bold text-darkblue">
+      <a href="/" className="flex flex-row items-center gap-[1vw]">
         <Image src={nesco} alt="NESCO" width={100} height={100} className="h-auto w-[6vw]" />
-        NESCO
-      </div>
+        <span className="text-[5vw] font-bold text-darkblue">NESCO</span>
+      </a>
       <div>
         <button onClick={() => toggleMainDropdown(!openDropdown)}>
           <IoIosArrowDown
@@ -121,73 +121,84 @@ const MobileMenu = ({ openDropdown, toggleMainDropdown, openChild, toggleChildDr
     </div>
 
     {/* Dropdown Menu */}
-    <motion.div
-      initial={{ opacity: 0, y: -20 }}
-      animate={{ opacity: openDropdown ? 1 : 0, y: openDropdown ? 0 : -20 }}
-      transition={{ ease: 'linear', duration: 0.3 }}
-    >
+    <AnimatePresence>
       {openDropdown && (
-        <ul className="absolute left-0 top-full z-10 flex w-full flex-row items-center justify-between border-b-2 border-white bg-lightyellow px-6 pb-2 text-[2.8vw] font-bold text-darkblue">
-          {routes.map((route, index) => (
-            <li key={index} className="group relative">
-              {route.child ? (
-                <>
-                  {/* Parent Dropdown */}
-                  <button
-                    onClick={() => toggleChildDropdown(openChild === index ? null : index)}
-                    onBlur={() => toggleChildDropdown(null)}
-                    className={`flex items-center gap-1 transition duration-200 ease-in-out hover:text-lightblue focus:outline-none ${openChild === index ? 'text-lightblue' : 'text-normal'}`}
+        <motion.div
+          // key="main-dropdown"
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -20 }}
+          transition={{ ease: 'linear', duration: 0.3 }}
+          className="absolute left-0 top-full z-10 w-full border-b-2 border-white bg-lightyellow"
+        >
+          <ul className="flex flex-row items-center justify-between px-6 pb-[2vw] text-[2.8vw] font-bold text-darkblue">
+            <button
+              type="button"
+              className="rounded-md bg-lightblue px-[2.5vw] py-1 font-bold text-darkblue transition duration-500 ease-in-out hover:bg-blue hover:text-lightyellow active:bg-darkyellow"
+            >
+              <Link href="/">
+                <p>SIGN IN</p>
+              </Link>
+            </button>
+            {routes.map((route, index) => (
+              <li key={index} className="group relative">
+                {route.child ? (
+                  <>
+                    {/* Parent Dropdown */}
+                    <button
+                      onClick={() => toggleChildDropdown(openChild === index ? null : index)}
+                      onBlur={() => toggleChildDropdown(null)}
+                      className={`flex items-center gap-1 transition duration-500 ease-in-out hover:text-lightblue focus:outline-none ${openChild === index ? 'text-lightblue' : 'text-normal'}`}
+                    >
+                      {route.name}
+                      <IoIosArrowDown
+                        className={`transition-transform duration-500 ease-in-out ${openChild === index ? 'rotate-180' : ''}`}
+                      />
+                    </button>
+
+                    {/* Child Dropdown */}
+                    <AnimatePresence>
+                      {openChild === index && (
+                        <motion.ul
+                          initial={{ height: 0, opacity: 0 }}
+                          animate={{ height: 'auto', opacity: 1 }}
+                          exit={{ height: 0, opacity: 0 }}
+                          transition={{ ease: 'linear', duration: 0.4 }}
+                          className="absolute left-[-40%] top-[170%] z-10 mt-2 w-fit rounded-lg bg-lightyellow px-[1.6vw] py-[1.6vw] shadow-lg sm:top-[150%]"
+                        >
+                          <div className="my-[0.5vw] h-[1.5vw] border-b-[0.5vw] border-t-[0.5vw] border-blue" />
+                          {route.child.map((child, childIndex) => (
+                            <motion.li
+                              key={childIndex}
+                              initial={{ opacity: 0, y: -5 }}
+                              animate={{ opacity: 1, y: 0 }}
+                              exit={{ opacity: 0, y: -5 }}
+                              transition={{ duration: 0.2, delay: childIndex * 0.1 }}
+                              className="text-nowrap pt-[0.6vw] !font-montserrat text-[2vw] text-darkblue duration-200 ease-in-out hover:text-lightblue"
+                            >
+                              <Link href={child.href} className="block">
+                                {child.name}
+                              </Link>
+                            </motion.li>
+                          ))}
+                        </motion.ul>
+                      )}
+                    </AnimatePresence>
+                  </>
+                ) : (
+                  <Link
+                    href={route.href}
+                    className="transition duration-500 ease-in-out hover:text-lightblue focus:outline-none"
                   >
                     {route.name}
-                    <IoIosArrowDown
-                      className={`transition-transform duration-500 ease-in-out ${openChild === index ? 'rotate-180' : ''}`}
-                    />
-                  </button>
-
-                  {/* Child Dropdown */}
-                  <motion.div
-                    initial={{ height: 0 }}
-                    animate={{
-                      height: openChild === index ? 'auto' : 0,
-                      opacity: openChild === index ? 1 : 0,
-                    }}
-                    transition={{ ease: 'linear', duration: 0.4 }}
-                  >
-                    {openChild === index && (
-                      <ul className="absolute top-[170%] z-10 mt-2 w-fit rounded-lg bg-lightyellow px-[1.6vw] py-[1.6vw] shadow-lg sm:top-[150%]">
-                        <div className="my-[0.5vw] h-[1.5vw] border-b-[0.5vw] border-t-[0.5vw] border-blue" />
-                        {route.child.map((child, childIndex) => (
-                          <li
-                            key={childIndex}
-                            className="text-nowrap pt-[0.6vw] !font-montserrat text-[2vw] text-darkblue duration-200 ease-in-out hover:text-lightblue"
-                          >
-                            <Link href={child.href} className="block">
-                              {child.name}
-                            </Link>
-                          </li>
-                        ))}
-                      </ul>
-                    )}
-                  </motion.div>
-                </>
-              ) : (
-                <Link
-                  href={route.href}
-                  className="transition duration-200 ease-in-out hover:text-lightblue focus:outline-none"
-                >
-                  {route.name}
-                </Link>
-              )}
-            </li>
-          ))}
-          <Link href="/">
-            <button className="rounded-sm bg-lightblue px-2 py-1 font-bold text-black transition duration-200 ease-in-out hover:bg-blue active:bg-darkyellow">
-              Sign In
-            </button>
-          </Link>
-        </ul>
+                  </Link>
+                )}
+              </li>
+            ))}
+          </ul>
+        </motion.div>
       )}
-    </motion.div>
+    </AnimatePresence>
   </div>
 );
 
@@ -202,29 +213,34 @@ export const Navbar = () => {
   const toggleChildDropdown = (index) => setopenChild(openChild === index ? null : index);
 
   return (
-    <nav className="relative left-0 top-0 z-50 w-full border-b-2 border-white bg-lightyellow font-kodeMono">
+    <nav className="fixed left-0 top-0 z-50 w-full border-b-2 border-white bg-lightyellow font-kodeMono">
       <div>
         {/* Logo */}
-        <div className="relative mx-auto hidden items-center justify-between px-6 md:flex">
-          <div className="my-[0.3vw] flex flex-row items-center gap-[1vw] text-[2.3vw] font-bold text-darkblue">
+        <div className="relative mx-auto hidden items-center justify-between px-[4vw] md:flex">
+          <a href="/" className="group flex cursor-pointer items-center space-x-3">
             <Image
               src={nesco}
               alt="NESCO"
               width={100}
               height={100}
-              className="h-auto w-[3.3vw] md:w-[2.8vw]"
+              className="h-auto w-[4.5vw] transition-transform duration-500 group-hover:scale-125 xl:w-[3.8vw]"
             />
-            NESCO
-          </div>
+            <span className="text-[3.5vw] font-bold text-darkblue transition-colors duration-500 group-hover:text-blue xl:text-[2.5vw]">
+              NESCO
+            </span>
+          </a>
 
           {/* Desktop Menu */}
           <DesktopMenu openDropdown={openDropdown} toggleMainDropdown={toggleMainDropdown} />
 
-          <Link href="/">
-            <button className="rounded-sm bg-lightblue px-2 py-[0.3vw] text-[1.8vw] font-bold text-black transition duration-500 ease-in-out hover:bg-blue active:bg-darkyellow 2xl:text-[1.2vw]">
-              Sign In
-            </button>
-          </Link>
+          <button
+            type="button"
+            className="rounded-md bg-lightblue px-[2.7vw] py-[0.5vw] text-[1.6vw] font-bold text-darkblue transition duration-500 ease-in-out hover:bg-blue hover:text-lightyellow active:bg-darkyellow xl:text-[1.1vw]"
+          >
+            <Link href="/">
+              <p>SIGN IN</p>
+            </Link>
+          </button>
         </div>
 
         {/* Mobile Menu */}
