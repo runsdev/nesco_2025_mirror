00d8f2749a984b1015072872a484f26c4fd8f8cb
FAN2257 <145684738+FAN2257@users.noreply.github.com>
2025-01-23 22:15:21 +0700

    feat(layout competition): mengubah styling pagination dan slider

--- modules/beranda/Competition/Competition.js
+++ modules/beranda/Competition/Competition.js
@@ -6,37 +6,45 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import { Pagination } from 'swiper/modules';
 import { agendaData } from '@/modules/data/data';
+import './competition.css';
 
 export const Competition = () => {
   return (
-    <div className="flex min-h-screen w-full flex-col items-center justify-center text-center text-[50px] font-[700]">
-      COMPETITION
-      <div className="relative w-[95%] lg:w-[85%]">
+    <div className="flex min-h-screen w-full flex-col items-center justify-center text-center">
+      <h1 className="font-kodeMono text-[50px] font-bold">Competitions</h1>
+      <div className="relative w-[100%]">
         <Swiper
           modules={[Pagination]}
           loop={true}
           pagination={{
             clickable: true,
             renderBullet: function (index, className) {
-              return '<span class="' + className + '"></span>';
+              return `
+                <span class="${className}">
+                </span>
+              `;
             },
           }}
-          className="h-[400px] w-full rounded-lg"
+          className="h-[80vh] w-[95%] rounded-xl lg:w-[85%]"
         >
           {agendaData.map((competition, index) => (
-            <SwiperSlide key={index}>
-              <div className="relative flex h-full w-full flex-col items-center justify-center">
-                <div className="relative flex h-[90%] w-[90%] items-center justify-center rounded-xl bg-white shadow-lg">
-                  <div className="h-[85%] w-[90%] rounded-lg bg-black p-5 text-white">
-                    <h2 className="mb-3 text-xl font-bold">{competition.title}</h2>
-                    <p className="text-sm">{competition.description}</p>
+            <SwiperSlide key={index} className="bg-gradient-to-b from-[#f0d478] to-[#addccf]">
+              <div className="relative flex h-[90%] w-full flex-col items-center justify-center">
+                <div className="relative flex h-[90%] w-[100%] items-center justify-center rounded-[40px] bg-white shadow-lg">
+                  <div className="h-[99%] w-[72%] rounded-[40px] bg-black px-5 py-5 text-white lg:py-14">
+                    <h2 className="mb-3 font-montserrat text-2xl font-bold text-[#EABB37] lg:text-2xl">
+                      {competition.title}
+                    </h2>
+                    <p className="text-justify font-montserrat text-sm text-white lg:text-2xl">
+                      {competition.description}
+                    </p>
                   </div>
                 </div>
+                <div className=""></div>
               </div>
             </SwiperSlide>
           ))}
         </Swiper>
-        {/* Custom Pagination */}
       </div>
     </div>
   );
--- modules/data/data.js
+++ modules/data/data.js
@@ -1,13 +1,20 @@
+import { title } from 'process';
+
 const agendaData = [
   {
     title: 'Paper Competition',
     description:
-      'Paper Competition is a competition where participants are required to write a scientific paper based on the theme that has been determined.',
+      'Paper Competition merupakan kompetisi bidang ketenagalistrikan berupa lomba problem solving tentang Load Flow Diagram, forecasting, sistem proteksi listrik, dan sejenisnya, yang bertujuan mendorong perkembangan dan perencanaan sistem ketenagalistrikan, khususnya di Indonesia.',
   },
   {
     title: 'Seminar',
     description:
       'Seminar is a discussion or conference that is usually held to discuss a particular topic.',
+  },
+  {
+    title: 'Workshop',
+    description:
+      'Workshop is a meeting at which a group of people engage in intensive discussion and activity on a particular subject or project.',
   },
 ];
 
