c33735d2a1daf9762b7b01a39304647cc77f51dc
FAN2257 <145684738+FAN2257@users.noreply.github.com>
2025-01-25 03:20:00 +0700

    fix(competition section): layout responsive

--- modules/beranda/Competition/Competition.js
+++ modules/beranda/Competition/Competition.js
@@ -7,44 +7,42 @@ import 'swiper/css/pagination';
 import { Pagination } from 'swiper/modules';
 import { agendaData } from '@/modules/data/data';
 import './competition.css';
+import greyRectangle from './icon/greyRectangle';
 
 export const Competition = () => {
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-center text-center">
       <h1 className="font-kodeMono text-[50px] font-bold">Competitions</h1>
-      <div className="relative w-[100%]">
+      <div className="relative flex w-[100%] flex-col items-center">
         <Swiper
           modules={[Pagination]}
           loop={true}
           pagination={{
             clickable: true,
             renderBullet: function (index, className) {
-              return `
-                <span class="${className}">
-                </span>
-              `;
+              return `<span class="${className}"></span>`;
             },
           }}
-          className="h-[80vh] w-[95%] rounded-xl lg:w-[85%]"
+          className="swiper-container h-[40vh] w-[95%] rounded-xl md:h-[80vh] lg:w-[70%]"
         >
           {agendaData.map((competition, index) => (
-            <SwiperSlide key={index} className="bg-gradient-to-b from-[#f0d478] to-[#addccf]">
+            <SwiperSlide key={index} className="transparent">
               <div className="relative flex h-[90%] w-full flex-col items-center justify-center">
                 <div className="relative flex h-[90%] w-[100%] items-center justify-center rounded-[40px] bg-white shadow-lg">
-                  <div className="h-[99%] w-[72%] rounded-[40px] bg-black px-5 py-5 text-white lg:py-14">
-                    <h2 className="mb-3 font-montserrat text-2xl font-bold text-[#EABB37] lg:text-2xl">
+                  <div className="h-[99%] w-[80%] rounded-[40px] bg-black px-5 py-5 text-white lg:w-[72%] lg:py-14">
+                    <h2 className="mb-3 font-montserrat text-xl font-[800] text-[#EABB37] md:text-2xl">
                       {competition.title}
                     </h2>
-                    <p className="text-justify font-montserrat text-sm text-white lg:text-2xl">
+                    <p className="text-justify font-montserrat text-sm font-[500] text-white md:text-2xl">
                       {competition.description}
                     </p>
                   </div>
                 </div>
-                <div className=""></div>
               </div>
             </SwiperSlide>
           ))}
         </Swiper>
+        <div className="swiper-pagination"></div>
       </div>
     </div>
   );
