39b37db9bd6578dec8faea0075199110b0a6d9c2
Monica@Ideapad-tasya <monicasyatasya@gmail.com>
2025-01-27 19:53:59 +0700

    feat(prizepool): membenarkan posisi

--- modules/beranda/Prizepool/Prizepool.js
+++ modules/beranda/Prizepool/Prizepool.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export const Prizepool = () => {
   return (
     <div className="relative flex w-full items-start justify-center max-lg:min-h-[50dvh] lg:min-h-screen">
-      {/* BINTANG, BULAN, METEOR */}
+      {/* BINTANG, BULAN, COMET */}
       <div className="flex flex-col items-center justify-center">
         <Image
           src="/prize/bintang.png"
@@ -18,7 +18,7 @@ export const Prizepool = () => {
           width={69}
           height={69}
           alt="moon"
-          className="mt-[-20vw] h-auto w-[15vw] md:mt-[-40vw] md:h-auto md:w-[12vw] lg:mt-[-40vw] lg:h-auto lg:w-[12vw]"
+          className="mt-[-20vw] h-auto w-[15vw] md:mt-[-50vw] md:h-auto md:w-[10vw] lg:mt-[-50vw] lg:h-auto lg:w-[9vw]"
         />
         <Image
           src="/prize/comets.png"
@@ -32,7 +32,7 @@ export const Prizepool = () => {
           width={810}
           height={456}
           alt="prizepool"
-          className="mt-[-20vw] h-auto w-[75vw] md:mt-[-30vw] md:h-auto md:w-[70vw] lg:mt-[-30vw] lg:h-auto lg:w-[70vw]"
+          className="relative bottom-[20vw] h-auto w-[80vw] md:bottom-[42vw] md:h-auto md:w-[70vw] lg:bottom-[42vw] lg:h-auto lg:w-[70vw]"
         />
       </div>
     </div>
