7e199a4f8c9bad8e820e72e1942fa332ac1f17c1
Nasta <khoirunasmuhammad@gmail.com>
2025-01-29 19:59:31 +0700

    fix(footer) benerin logo footer

--- components/UI/Footer.js
+++ components/UI/Footer.js
@@ -2,6 +2,7 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
+import LogoNesco from '../Element/LogoNesco';
 
 const quickLinks = [
   { href: '/competition/debate', label: 'Debate Competition' },
@@ -18,39 +19,36 @@ const socialLinks = [
   { href: 'https://twitter.com/nesco', label: 'Twitter' },
   { href: 'https://linkedin.com/company/nesco', label: 'LinkedIn' },
 ];
+
+const TulisanNesco = () => {
+  return (
+    <div className="group relative aspect-[163/71] w-[50%] overflow-clip">
+      <LogoNesco className="absolute bottom-0 left-0 top-0 my-auto w-[31.9%] transition duration-500 group-hover:scale-110" />
+      <div className="absolute bottom-0 right-0 top-0 my-auto mt-[4%] font-kodeMono text-[9.5vw] font-bold text-lightyellow transition duration-500 group-hover:text-white md:text-[3.5vw]">
+        NESCO
+      </div>
+      <div className="absolute bottom-0 right-0 top-0 mt-[27%] font-kodeMono text-[2.8vw] font-bold text-lightyellow transition duration-500 group-hover:text-white md:mt-[25%] md:text-[1.4vw]">
+        2025
+      </div>
+    </div>
+  );
+};
+
 export const Footer = () => {
   return (
-    <footer className="relative w-full bg-[#003C43]">
-      <div className="container mx-auto flex max-w-screen-2xl flex-col justify-between px-8 py-10 md:flex-row">
+    <footer className="relative flex w-full flex-row items-center justify-center bg-[#003C43]">
+      <div className="mb-[20%] flex w-[90%] flex-col justify-between space-x-0 space-y-[3%] py-[1%] md:mb-[0.5%] md:flex-row md:space-x-[1%] md:space-y-0">
         {/* Logo dan Nesco*/}
-        <div className="flex-[3] px-5 py-3 md:py-0">
-          <Link href="/" className="group flex flex-row items-center">
-            <div>
-              <Image
-                src="/apple-touch-icon.png"
-                alt="Logo NESCO"
-                height={90}
-                width={90}
-                className="transition duration-500 group-hover:scale-110"
-              />
-            </div>
-            <div className="pl-4 pt-3 transition duration-500 group-hover:scale-110">
-              <p className="font-kodeMono text-[8vw] font-bold text-[#FEDF8D] transition duration-500 group-hover:text-white md:text-6xl">
-                NESCO
-              </p>
-              <p className="text-right font-kodeMono text-[3vw] font-bold text-[#FEDF8D] transition duration-500 group-hover:text-white md:text-sm">
-                2025
-              </p>
-            </div>
-          </Link>
+        <div className="flex-[3]">
+          <TulisanNesco />
         </div>
 
         {/* Quick Links */}
-        <div className="flex-[2] px-5 py-3 text-left md:py-0">
-          <h3 className="mb-2 font-montserrat text-[3vw] font-bold text-white md:text-xl">
+        <div className="flex-[2] py-[1%] text-left md:px-[1%] md:py-0">
+          <h3 className="mb-[3%] font-montserrat text-[5vw] font-bold text-white md:text-[1.2vw]">
             Quick Links
           </h3>
-          <div className="flex flex-col space-y-2 text-[2vw] md:text-sm">
+          <div className="flex flex-col space-y-[3%] text-[4vw] md:space-y-[1%] md:text-[0.9vw]">
             {quickLinks.map((link) => (
               <Link
                 key={link.href}
@@ -64,11 +62,11 @@ export const Footer = () => {
         </div>
 
         {/* Find Us */}
-        <div className="flex-1 px-5 py-3 text-left md:py-0">
-          <h3 className="mb-2 font-montserrat text-[3vw] font-bold text-white md:text-xl">
+        <div className="flex-[1] py-[1%] text-left md:px-[1%] md:py-0">
+          <h3 className="mb-[3%] font-montserrat text-[5vw] font-bold text-white md:text-[1.2vw]">
             Find Us
           </h3>
-          <div className="flex flex-col space-y-2 text-[2vw] md:text-sm">
+          <div className="flex flex-col space-y-[3%] text-[4vw] md:space-y-[1%] md:text-[0.9vw]">
             {socialLinks.map((link) => (
               <Link
                 key={link.href}
@@ -82,13 +80,13 @@ export const Footer = () => {
         </div>
 
         {/* Contact Us */}
-        <div className="flex-1 px-5 py-3 pb-20 text-left sm:pb-24 md:py-0">
-          <h3 className="mb-2 font-montserrat text-[3vw] font-bold text-white md:text-xl">
+        <div className="flex-[1] py-[1%] text-left md:px-[1%] md:py-0">
+          <h3 className="mb-[3%] font-montserrat text-[5vw] font-bold text-white md:text-[1.2vw]">
             Contact Us
           </h3>
           <Link
             href="mailto:nesco@mail.ugm.ac.id"
-            className="font-montserrat text-[2vw] text-[#69C7BF] transition-colors hover:text-white md:text-sm"
+            className="font-montserrat text-[4vw] text-[#69C7BF] transition-colors hover:text-white md:text-[0.9vw]"
           >
             nesco@mail.ugm.ac.id
           </Link>
@@ -98,7 +96,7 @@ export const Footer = () => {
       <div className="absolute bottom-0 left-0 w-full justify-start md:w-[40%]">
         <Image
           src="/Union.png"
-          alt="Kota"
+          alt="Cityscape"
           width={760}
           height={101}
           className="w-full object-cover"
