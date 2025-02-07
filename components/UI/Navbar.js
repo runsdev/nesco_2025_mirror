b40d28c6e31dcc697c66027cd1c1fa80f1eda8ea
mie-intel <paryapradhanika@gmail.com>
2025-02-07 21:10:33 +0700

    fix(LocomotiveProvider): memperbaiki locomotive

--- components/UI/Navbar.js
+++ components/UI/Navbar.js
@@ -51,9 +51,9 @@ const DesktopMenu = ({ openDropdown, toggleMainDropdown }) => (
         {route.child ? (
           <>
             <button
-              className={`flex w-full items-center gap-1 capitalize transition duration-200 ease-in-out focus:outline-none ${openDropdown === index ? 'text-lightblue' : 'text-normal'}`}
+              className={`flex w-full items-center gap-1 capitalize transition duration-200 ease-in-out hover:text-lightblue focus:outline-none ${openDropdown === index ? 'text-lightblue' : 'text-normal'}`}
               onClick={() => toggleMainDropdown(openDropdown === index ? null : index)}
-              onMouseEnter={() => toggleMainDropdown(index)}
+              // onMouseEnter={() => toggleMainDropdown(index)}
             >
               {route.name}
               <IoIosArrowDown
@@ -72,7 +72,7 @@ const DesktopMenu = ({ openDropdown, toggleMainDropdown }) => (
                 height: openDropdown === index ? 'auto' : 0,
                 opacity: openDropdown === index ? 1 : 0,
               }}
-              transition={{ ease: 'linear', duration: 0.4 }}
+              transition={{ ease: 'linear', duration: 0.27 }}
             >
               <ul>
                 <div className="my-[0.6vw] h-[0.8vw] border-b-[0.3vw] border-t-[0.3vw] border-chart-2" />
