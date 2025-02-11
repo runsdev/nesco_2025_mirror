676f8548ef0cf06dfcd09c6ff29cb7d65c3b983c
Nasta <khoirunasmuhammad@gmail.com>
2025-02-11 11:50:15 +0700

    feat(seminar) add seminar

--- modules/beranda/Seminar/Seminar.js
+++ modules/beranda/Seminar/Seminar.js
@@ -4,26 +4,29 @@ import Image from 'next/image';
 const GunungAwanPohon = () => {
   return (
     <div className="relative w-full">
-      <Image
-        src="/seminar/trees kiri.png"
-        alt="Tress kiri"
-        width={1649}
-        height={1917}
-        className="absolute z-[10] mt-[-65%] w-[40%] md:mt-[-60%]"
-      />
-      <Image
-        src="/seminar/trees kanan.png"
-        alt="Tress kanan"
-        width={1649}
-        height={1917}
-        className="absolute right-0 z-[10] mt-[-65%] w-[40%] md:mt-[-60%]"
-      />
+      <div className="relative overflow-x-clip">
+        <Image
+          src="/seminar/trees kiri.png"
+          alt="Tress kiri"
+          width={1649}
+          height={1917}
+          className="absolute z-[10] ml-[-6%] mt-[-68%] w-[40%] scale-[155%] md:ml-[-6%] md:mt-[-55%] md:scale-105"
+        />
+        <Image
+          src="/seminar/trees kanan.png"
+          alt="Tress kanan"
+          width={1649}
+          height={1917}
+          className="absolute right-0 z-[10] mr-[-6%] mt-[-68%] w-[40%] scale-[155%] md:mr-[-6%] md:mt-[-55%] md:scale-105"
+        />
+      </div>
+
       <Image
         src="/seminar/Gunung.png"
         alt="Gunung"
         width={2250}
         height={430}
-        className="relative z-[99] w-full"
+        className="relative z-[99] mt-[2%] w-full md:mt-[4%]"
       />
       <Image
         src="/seminar/Awan.png"
@@ -44,11 +47,11 @@ export const Seminar = () => {
         <div className="pb-[5%] text-center font-kodeMono text-[6vw] font-bold md:gap-[4vw] md:text-[5vw]">
           {seminarData.title}
         </div>
-        <div className="pb-[5%] text-justify font-montserrat text-[2vw] text-[black] md:text-balance md:text-[1.5vw]">
+        <div className="pb-[5%] text-justify font-montserrat text-[2.5vw] text-[black] md:text-balance md:text-[1.5vw]">
           {seminarData.description}
         </div>
       </div>
-      <GunungAwanPohon />
+      <GunungAwanPohon className="mt-[-5%]" />
     </div>
   );
 };
