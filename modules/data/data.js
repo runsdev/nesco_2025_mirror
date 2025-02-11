21c01d292f6ae0c5d01e67066a654278c5f74396
mie-intel <paryapradhanika@gmail.com>
2025-02-11 17:07:51 +0700

    fix(data): memperbaiki data

--- modules/data/data.js
+++ modules/data/data.js
@@ -2,7 +2,22 @@ const agendaData = [
   {
     title: 'Paper Competition',
     description:
-      'Paper Competition is a competition where participants are required to write a scientific paper based on the theme that has been determined.',
+      'Paper Competition NESCO 2025 merupakan ajang yang dirancang untuk menguji kemampuan problem-solving peserta dalam menganalisis dan memberikan solusi atas permasalahan di bidang ketenagalistrikan dan sistem tenaga listrik.',
+  },
+  {
+    title: 'Innovation Challenge',
+    description:
+      'Innovation Challenge Competition NESCO 2025 adalah kompetisi di bidang ketenagalistrikan yang berfokus pada karya inovatif yang menawarkan solusi kreatif terhadap permasalahan yang diberikan.',
+  },
+  {
+    title: 'Poster',
+    description:
+      'Poster Competition NESCO 2025 adalah kompetisi yang dirancang untuk memberikan platform bagi siswa SMA (Junior) dan mahasiswa (Senior) untuk mengungkapkan ide dan pemikiran mereka mengenai berbagai isu dan inovasi di bidang ketenagalistrikan dan energi terbarukan melalui media visual digital.',
+  },
+  {
+    title: 'Scientific Debate',
+    description:
+      'Scientific Debate Competition NESCO 2025 adalah kompetisi yang dirancang untuk mengasah kemampuan berpikir kritis, analitis, dan argumentatif para peserta dalam menyikapi isu-isu terkini di bidang ketenagalistrikan dan keberlanjutan lingkungan.',
   },
   {
     title: 'Seminar',
