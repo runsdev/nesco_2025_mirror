af9997e6b77e92f63292fa08965aa57aadfacb49
mie-intel <paryapradhanika@gmail.com>
2025-03-08 17:10:43 +0700

    feat(ErrorPage): add error page

---
+++ app/(main)/seminar/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import ErrorPage from '@/components/ErrorPage';
+
+export default function Page() {
+  return <ErrorPage pesan="SOON" text1="Halaman Belum Tersedia :(" text2="Kembali ke Beranda" />;
+}
