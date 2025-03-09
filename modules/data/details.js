58eef15c9a93cf120caf83649343700397b4b116
Harun <harunsixsixfour@gmail.com>
2025-03-09 20:53:57 +0700

    fix: fixing the info

--- modules/data/details.js
+++ modules/data/details.js
@@ -2,9 +2,9 @@ const details = {
   debate: {
     title: 'Scientific Debate',
     description:
-      'Paper Competition merupakan kompetisi bidang ketenagalistrikan berupa lomba problem solving tentang Load Flow Diagram, forecasting, sistem proteksi listrik, dan sejenisnya, yang bertujuan mendorong perkembangan dan perencanaan sistem ketenagalistrikan, khususnya di Indonesia.',
+      'Optimization of Technology in Supporting Renewable Energy Integration. \n\nPaper Competition merupakan kompetisi bidang ketenagalistrikan berupa lomba problem solving tentang Load Flow Diagram, forecasting, sistem proteksi listrik, dan sejenisnya, yang bertujuan mendorong perkembangan dan perencanaan sistem ketenagalistrikan, khususnya di Indonesia.',
     kategori: {
-      jenis: 'Mahasiswa',
+      jenis: 'Mahasiswa S1/D4/D3/D2/D1',
       tim: 'Perorangan',
       jumlah: 'Tim maksimal 3 orang',
     },
@@ -14,13 +14,13 @@ const details = {
       line: 'aafifahyunii',
       line2: 'hazellut',
     },
-    biaya: 'Rp 100.000',
+    biaya: 'Rp 300.000',
     guidebook: 'Link',
   },
   innovation: {
     title: 'Innovation Challenge',
     description:
-      'Innovation Challenge Competition NESCO 2025 adalah kompetisi di bidang ketenagalistrikan yang berfokus pada karya inovatif yang menawarkan solusi kreatif terhadap permasalahan yang diberikan.',
+      'Driving Sustainable Transformation: Accelerating Renewable Energy Solutions for a Resilient Indonesia. \n\nInnovation Challenge Competition NESCO 2025 adalah kompetisi di bidang ketenagalistrikan yang berfokus pada karya inovatif yang menawarkan solusi kreatif terhadap permasalahan yang diberikan.',
     kategori: {
       jenis: 'Mahasiswa',
       tim: 'Perorangan',
@@ -38,9 +38,9 @@ const details = {
   paper: {
     title: 'Paper Competition',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl nec lacinia lacinia, nunc nisl lacinia nisl, nec lacinia nisl nisl nec nisl. Nullam auctor, nisl nec lacinia lacinia, nunc nisl lacinia nisl, nec lacinia nisl nisl nec nisl.',
+      'Optimization of Technology in Supporting Renewable Energy Integration. \n\nPaper Competition NESCO 2025 merupakan ajang perlombaan yang dirancang untuk menguji kemampuan problem-solving peserta dalam menganalisis dan memberikan solusi atas permasalahan di bidang ketenagalistrikan dan sistem tenaga listrik.',
     kategori: {
-      jenis: 'Mahasiswa',
+      jenis: 'Mahasiswa S1/D4/D3/D2/D1',
       tim: 'Perorangan',
       jumlah: 'Tim maksimal 3 orang',
     },
@@ -56,9 +56,9 @@ const details = {
   poster: {
     title: 'Poster Competition',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl nec lacinia lacinia, nunc nisl lacinia nisl, nec lacinia nisl nisl nec nisl. Nullam auctor, nisl nec lacinia lacinia, nunc nisl lacinia nisl, nec lacinia nisl nisl nec nisl.',
+      'Poster Competition NESCO 2025 adalah kompetisi yang dirancang untuk memberikan platform bagi siswa SMA (Junior) dan mahasiswa (Senior) untuk mengungkapkan ide dan pemikiran mereka mengenai berbagai isu dan inovasi di bidang ketenagalistrikan dan energi terbarukan melalui media visual digital.',
     kategori: {
-      jenis: 'Mahasiswa',
+      jenis: 'Mahasiswa S1/D dan SMA/Sederajat',
       tim: 'Perorangan',
       jumlah: 'Tim maksimal 3 orang',
     },
@@ -68,7 +68,7 @@ const details = {
       line: 'johanesbimo',
       line2: 'putraparindra_',
     },
-    biaya: 'Rp 100.000',
+    biaya: 'Rp 50.000 / Rp 80.000',
     guidebook: 'Link',
   },
 };
