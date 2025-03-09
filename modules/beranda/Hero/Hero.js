389ede4fa1190d93df5efd7ea0f537e2881411d8
mie-intel <paryapradhanika@gmail.com>
2025-03-09 10:19:18 +0700

    feat(Hero Splash Screen)

--- modules/beranda/Hero/Hero.js
+++ modules/beranda/Hero/Hero.js
@@ -17,7 +17,8 @@ export const Hero = () => {
       {/* Tulisan dan Logo Nesco */}
       <div
         data-aos="zoom-in"
-        data-aos-duration="1000"
+        data-aos-duration="1700"
+        data-aos-delay="1000"
         className="absolute left-0 right-0 z-[10] mx-auto aspect-[354/250] w-[90%] max-lg:top-[7%] sm:w-[80%] md:w-[52%] lg:top-[30%] lg:aspect-[1380/374] lg:w-[71.8%]"
       >
         <div className="absolute left-0 aspect-[967/185] w-full max-lg:bottom-0 lg:top-[25%] lg:w-[70%]">
@@ -34,7 +35,8 @@ export const Hero = () => {
       </div>
       <button
         data-aos="zoom-in"
-        data-aos-duration="1000"
+        data-aos-duration="1700"
+        data-aos-delay="1000"
         type="button"
         className="absolute top-[69%] z-[20] w-[47%] overflow-clip rounded-3xl bg-gradient-to-r from-[#003C43] via-[#0F776E] to-[#61CCC2] bg-[length:200%_200%] bg-[position:top_left] text-xl text-lightyellow transition-all duration-500 hover:bg-[position:bottom_right] hover:text-white sm:w-[37%] md:top-[61%] md:w-[32%] lg:top-[59%] lg:w-[230px] lg:rounded-3xl 2xl:top-[64%]"
       >
@@ -47,17 +49,35 @@ export const Hero = () => {
       </button>
 
       {/* Gunung kiri */}
-      <div className="absolute bottom-[8vw] left-[-14%] z-[8] aspect-[1194/1450] w-[58%] md:left-[-13%] md:w-[55%] lg:left-[-6%] lg:w-[29.5%]">
+      <div
+        data-aos="fade-right"
+        data-aos-delay="1000"
+        className="absolute bottom-[8vw] left-[-14%] z-[8] aspect-[1194/1450] w-[58%] md:left-[-13%] md:w-[55%] lg:left-[-6%] lg:w-[29.5%]"
+      >
         <Image alt="gunung nesco kiri" src="/assets/beranda/Hero/gunung-kiri.png" fill />
       </div>
-      <div className="absolute bottom-[4vw] right-[-25%] z-[8] aspect-[631/728] w-[68%] md:right-[-24%] md:w-[70%] lg:right-[-8%] lg:w-[34%]">
+      <div
+        data-aos="fade-left"
+        data-aos-delay="900"
+        className="absolute bottom-[4vw] right-[-25%] z-[8] aspect-[631/728] w-[68%] md:right-[-24%] md:w-[70%] lg:right-[-8%] lg:w-[34%]"
+      >
         <Image alt="gunung nesco kanan" src="/assets/beranda/Hero/gunung-kanan.png" fill />
       </div>
       {/* Gunung kanan */}
-      <div className="absolute bottom-[14vw] w-full lg:bottom-[5vw]">
+      <div
+        data-aos="fade-up"
+        data-aos-delay="500"
+        className="absolute bottom-[14vw] z-[9] w-full lg:bottom-[5vw]"
+      >
         <TanahAtas />
       </div>
-      <div className="absolute bottom-0 w-full">
+      <div
+        data-aos="fade-up"
+        data-aos-delay="500"
+        className="absolute bottom-0 z-[8] aspect-[25/5] w-full bg-lightyellow lg:hidden"
+      />
+
+      <div data-aos="fade-up" className="absolute bottom-0 z-[12] w-full">
         <TanahBawah />
       </div>
     </section>
