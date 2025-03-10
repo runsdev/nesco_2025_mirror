b35ce2b73df66e1a51c834e7ea1592279750885c
Harun <harunsixsixfour@gmail.com>
2025-03-10 08:05:28 +0700

    fix: move main page to root

---
+++ app/layout.tsx
@@ -0,0 +1,53 @@
+// import HeaderAuth from '@/components/header-auth';
+// import { ThemeSwitcher } from '@/components/theme-switcher';
+// import { Geist } from 'next/font/google';
+// import { ThemeProvider } from 'next-themes';
+
+import { Kode_Mono } from 'next/font/google';
+import { Montserrat } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/next';
+
+const kodeMono = Kode_Mono({
+  display: 'swap',
+  weight: ['400', '500', '600', '700'],
+  subsets: ['latin'],
+});
+
+const montserrat = Montserrat({
+  display: 'swap',
+  weight: ['300', '400', '500', '600', '700'],
+  subsets: ['latin'],
+});
+
+import LocomotiveProvider from '@/providers/LocomotiveProvider';
+import GSAPProvider from '@/providers/GSAPProvider';
+import { Navbar, Footer } from '@/components/UI/index';
+import '@/styles/globals.css';
+import '@/styles/animasi.css';
+
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : 'http://localhost:3000';
+
+export const metadata = {
+  metadataBase: new URL(defaultUrl),
+  title: 'NESCO UGM',
+  description: 'National Electrical Power System Competition 2025',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${kodeMono.className} ${montserrat.className}`}>
+        <LocomotiveProvider>
+          <GSAPProvider>
+            <Navbar />
+            {children}
+            <Analytics />
+            <Footer />
+          </GSAPProvider>
+        </LocomotiveProvider>
+      </body>
+    </html>
+  );
+}
